f821d32f31d9fd5c32e50e3ffc824c8e8ba7e29e
Maxim Dementyev <max@orofarne.ru>
2015-05-09 20:02:42 +0300

    idx structure

---
+++ thrift/idx.thrift
@@ -0,0 +1,15 @@
+namespace cpp idx
+
+struct Tile {
+	1: i64 offset,
+	2: i64 size,
+	3: i64 capacity
+}
+
+struct File {
+	1: i16 min_zoom,
+	2: i16 max_zoom,
+	3: i64 coord_x,
+	4: i64 coord_y,
+	5: list<Tile> tiles
+}
